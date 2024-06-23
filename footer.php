51c6a6e0218cafc786847f7f20d0d250acd772c8
jaycee808 <jamie.clark@live.co.uk>
2024-06-23 14:09:10 +0100

    updated front page

--- footer.php
+++ footer.php
@@ -1,7 +1,10 @@
 <!-- footer -->
 <footer class="page-footer">
     <div class="footer-content">
-        <div class="footer-text">Copyright &copy; 4WARD 2GETHER. Registered Charity No. SC053309</div>
+        <div class="footer-text">
+            Copyright &copy; 4WARD 2GETHER.
+            <br>
+            Registered Charity No. SC053309</div>
     </div>
 </footer>
 <?php wp_footer(); ?>
